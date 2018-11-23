cee282c7a65725ffe600031bd02666618f607b41
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2018-11-23 11:51:08 -0300

    no need for making a variable we dont use

--- scrapyohio/scrapyohio/settings.py
+++ scrapyohio/scrapyohio/settings.py
@@ -28,10 +28,9 @@ SPIDER_MODULES = ['scrapyohio.spiders']
 NEWSPIDER_MODULE = 'scrapyohio.spiders'
 
 
-ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
     # 'scrapyohio.pipelines.PricePipeline': 300,
-    # 'myproject.pipelines.JsonWriterPipeline': 800,
-}
+# }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'scrapyohio (+http://www.yourdomain.com)'
