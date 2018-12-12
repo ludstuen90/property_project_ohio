2a1b25a7518b2f71c4c7dc2114d70562a5acfff3
Lukas Udstuen <lukas.udstuen@gmail.com>
2018-12-12 10:42:39 -0300

    Update warren_mortgage.py

--- scrapyohio/scrapyohio/scraper_helpers/warren_mortgage.py
+++ scrapyohio/scrapyohio/scraper_helpers/warren_mortgage.py
@@ -38,8 +38,8 @@ class WarrenMortgageInfo:
         # warren.py
         self.warren_county_object = models.County.objects.get(name='Warren')
 
-        # self.warren_county_items = models.Property.objects.filter(county=self.warren_county_object)[:5]
-        self.warren_county_items = models.Property.objects.filter(id=257453)
+        self.warren_county_items = models.Property.objects.filter(county=self.warren_county_object)
+        # self.warren_county_items = models.Property.objects.filter(id=257453)
         self.access_token = ''
 
     @classmethod
